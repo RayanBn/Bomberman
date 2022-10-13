5d18986c03134c39dc4cd9906a40c8634da29839
rayan <bannirayan@gmail.com>
2022-10-13 15:05:43 +0200

    [ADD] PlayerMovement

--- main.c
+++ main.c
@@ -5,57 +5,104 @@ int eventLoop(void)
     SDL_Event event;
 
     if (SDL_PollEvent(&event)) {
-        if (event.type == SDL_QUIT) {
+        if (event.type == SDL_QUIT)
             return LEAVE;
-        }
     }
     return GAME;
 }
 
 char *readMap(char *filepath)
 {
-    int mapSize = 120;
+    int mapSize = 255;
     char *string = malloc(sizeof(char) * mapSize);
     int fd = open(filepath, O_RDONLY);
 
     read(fd, string, mapSize);
     close(fd);
-    write(1, string, 120);
     return string;
 }
 
-void createMap(SDL_Texture *texture, SDL_Renderer *renderer)
+SDL_Rect checkTexture(char c)
 {
-    int nbMapColumns = 20;
-    int nbMapLines = 6;
-    SDL_Texture ***textureMap = malloc(sizeof(SDL_Texture **) * (nbMapLines + 1));
-    SDL_Rect rectSize = {0, 0, 64, 64};
-    SDL_Rect myRect = {16 * 6, 0, 16, 16};
+    SDL_Rect rect;
+
+    if (c == 'x')
+        rect = (SDL_Rect){16 * 0, 16 * 0, 16, 16};
+    if (c == '.')
+        rect = (SDL_Rect){120, 16 * 1, 16, 16};
+    if (c == 'b')
+        rect = (SDL_Rect){16 * 2, 16 * 0, 16, 16};
+    return rect;
+}
+
+void renderMap(SDL_Renderer *renderer, SDL_Texture *texture, SDL_Rect **mapRects, char *textMap)
+{
+    SDL_Rect rectTexture;
+    int countChar = 0;
+
+    for (int y = 0; y < 11; y++) {
+        for (int x = 0; x < 15; x++) {
+            rectTexture = checkTexture(textMap[countChar]);
+            SDL_RenderCopy(renderer, texture, &rectTexture, &mapRects[y][x]);
+            countChar += 1;
+        }
+        countChar += 1;
+    }
+}
+
+SDL_Rect **createMap(void)
+{
+    int nbMapColumns = 15;
+    int nbMapLines = 11;
+    int rectSize = WIDTH / 15; 
+    SDL_Rect **textureMap = malloc(sizeof(SDL_Rect *) * (nbMapLines + 1));
+    SDL_Rect rectPos = {0, 0, rectSize, rectSize};
     int posx = 0;
     int posy = 0;
 
     for (int y = 0; y < nbMapLines; y++) {
-        textureMap[y] = malloc(sizeof(SDL_Texture *) * (nbMapColumns + 1));
+        textureMap[y] = malloc(sizeof(SDL_Rect) * (nbMapColumns + 1));
+        rectPos.y = posy;
         for (int x = 0; x < nbMapColumns; x++) {
-            rectSize.x = posx;
-            SDL_RenderCopy(renderer, texture, &myRect, &rectSize);
-            SDL_RenderPresent(renderer);
-            posx += 64;
+            rectPos.x = posx;
+            textureMap[y][x] = rectPos;
+            posx += rectSize;
         }
-        rectSize.y = posy;
-        posy += 64;
+        posy += rectSize;
         posx = 0;
     }
+    return textureMap;
 }
 
-int main (void)
+SDL_Rect playerMovement(SDL_Rect playerPos)
+{
+    SDL_Event event;
+    int speed = 10;
+
+    if (SDL_PollEvent(&event)) {
+        if (event.key.keysym.sym == SDLK_LEFT)
+            playerPos.x -= speed;
+        if (event.key.keysym.sym == SDLK_RIGHT)
+            playerPos.x += speed;
+        if (event.key.keysym.sym == SDLK_UP)
+            playerPos.y -= speed;
+        if (event.key.keysym.sym == SDLK_DOWN)
+            playerPos.y += speed;
+    }
+    return playerPos;
+}
+
+int main(void)
 {
     SDL_Window *window = NULL;
     int status = GAME;
     SDL_Renderer *renderer;
     SDL_Surface *mapTileset;
     SDL_Texture *texture;
-    readMap("./maps/map1.txt");
+    SDL_Rect **mapRects = createMap();
+    SDL_Rect playerRect = {0, 32, 10, 16};
+    SDL_Rect playerPos = {60, 50, 10 * 4, 16 * 4};
+    char *textMap = readMap("./maps/map1.txt");
 
     window = SDL_CreateWindow("Bomberman", 0, 0, WIDTH, HEIGHT, 0);
     renderer = SDL_CreateRenderer(window, -1, 0);
@@ -63,7 +110,10 @@ int main (void)
     texture = SDL_CreateTextureFromSurface(renderer, mapTileset);
     while (status != LEAVE) {
         status = eventLoop();
-        createMap(texture, renderer);
+        renderMap(renderer, texture, mapRects, textMap);
+        playerPos = playerMovement(playerPos);
+        SDL_RenderCopy(renderer, texture, &playerRect, &playerPos);
+        SDL_RenderPresent(renderer);
     }
     SDL_DestroyTexture(texture);
     SDL_FreeSurface(mapTileset);
